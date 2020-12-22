94c5cd532e6bffb69abe29b6b383a3b6a5737224
asifraza2081 <76470551+asifraza2081@users.noreply.github.com>
2020-12-22 19:23:13 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+          
+            $('.owl-carousel').owlCarousel({
+       loop:true,
+       margin:20,
+       responsiveClass:true,
+       nav:false,
+       dots:true,
+       autoplay:true,
+       rewind:true,
+       autoplaytimeout:5000,
+       autoplayHoverPause:true,
+       responsive:{
+         0:{
+              items:1,
+              },
+          600:{
+              items:3,
+               },
+         1000:{
+             items:4,
+               }
+     }
+  
+  });
+
+
+            $(".menu-toggle").click(function(){
+                $(".main-menu").fadeToggle();
+            });
+
+            
+
+
+        });
